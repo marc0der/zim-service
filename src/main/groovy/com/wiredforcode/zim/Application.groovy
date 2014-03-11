3ff1b5a49eb72edbb04fb663a0a3f44338d233d1
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-03-11 08:46:31 +0000

    Trim excessive code from application bootstrap.

--- src/main/groovy/com/wiredforcode/zim/Application.groovy
+++ src/main/groovy/com/wiredforcode/zim/Application.groovy
@@ -1,14 +1,11 @@
 package com.wiredforcode.zim
 
-import com.wiredforcode.zim.config.MongoConfiguration
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Import
 
 @EnableAutoConfiguration
-@ComponentScan("com.wiredforcode.zim.controllers")
-@Import(MongoConfiguration)
+@ComponentScan("com.wiredforcode.zim")
 class Application {
     static void main(String[] args){
         new SpringApplication(Application).run(args)
