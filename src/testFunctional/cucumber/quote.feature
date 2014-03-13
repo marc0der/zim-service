a052ccb03c8309a2f42fe072559e5d7f04ce6646
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-03-13 18:53:29 +0000

    Adds embedded mongo to the mix, fixes functional tests.

--- src/testFunctional/cucumber/quote.feature
+++ src/testFunctional/cucumber/quote.feature
@@ -2,8 +2,8 @@ Feature: Invader Quotes
 
   Scenario: An Invader quotes by Name
     Given an Invader named "GIR"
-    And an Invader named "zim"
+    And an Invader named "Zim"
     And the Invader "GIR" has a quote "Can I be a mongoose dog?"
-    And the Invader "zim" has a quote "The Earth is safe! I did it, GIR! Now let's go destroy it!"
+    And the Invader "Zim" has a quote "The Earth is safe! I did it, GIR! Now let's go destroy it!"
     When a Quote is requested for "GIR"
     Then we hear "Can I be a mongoose dog?"
--- src/testFunctional/steps/quote.groovy
+++ src/testFunctional/steps/quote.groovy
@@ -1,3 +1,6 @@
+import com.mongodb.BasicDBObject
+import com.mongodb.MongoClient
+import com.mongodb.WriteConcern
 import wslite.rest.RESTClient
 
 import static cucumber.api.groovy.EN.And
@@ -5,25 +8,27 @@ import static cucumber.api.groovy.Hooks.Before
 
 Before(){
     client = new RESTClient("http://localhost:8080")
+    def mongo = new MongoClient()
+    mongo.writeConcern = WriteConcern.NORMAL
+    def db = mongo.getDB("invasion")
+    quotes = db.createCollection("quote", new BasicDBObject())
 }
 
 And(~'^an Invader named "([^"]*)"$') { String invader ->
-    // Express the Regexp above with the code you wish you had
-    //throw new cucumber.api.PendingException()
+    quotes.insert(new BasicDBObject("name", invader))
 }
 
-And(~'^the Invader "([^"]*)" has a quote "([^"]*)"$') { String invader, String quote ->
-    // Express the Regexp above with the code you wish you had
-    //throw new cucumber.api.PendingException()
+And(~'^the Invader "([^"]*)" has a quote "([^"]*)"$') { String invader, String message ->
+    def query = new BasicDBObject("name", invader)
+    def update = new BasicDBObject("name", invader).append("message", message)
+    quotes.update(query, update)
 }
 
 And(~'^a Quote is requested for "([^"]*)"$') { String invader ->
-    // Express the Regexp above with the code you wish you had
     response = client.get(path:"/invasion/${invader}")
 }
 
 And(~'^we hear "([^"]*)"$') { String quote ->
-    // Express the Regexp above with the code you wish you had
     assert response.statusCode == 200
     assert response.json.message == quote
 }
