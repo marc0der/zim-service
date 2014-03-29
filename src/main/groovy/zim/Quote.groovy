3f7add7d014a4388178267a65b7d8d6a69d55811
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-03-29 17:19:49 +0100

    Remove spring data mongo annotations.

--- src/main/groovy/zim/Quote.groovy
+++ src/main/groovy/zim/Quote.groovy
@@ -1,12 +1,7 @@
 package zim
 
-import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.mapping.Document
-import org.springframework.data.mongodb.core.mapping.Field
-
-@Document
 class Quote {
-    @Id BigInteger id
-    @Field String name
-    @Field String message
+    BigInteger id
+    String name
+    String message
 }
